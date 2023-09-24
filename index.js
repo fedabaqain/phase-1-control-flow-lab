2b3a7a68558b240882f06e81937abb53434ea054
fedabeqain <feda.beqain@gmail.com>
2023-09-25 00:46:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) 
+  {
+    return 'This one is on me!'
+  }
+  else if (ride > 400 && ride < 2000)
+  
+  {
+    return 'That will be twenty bucks.'
+  }  
+else if (ride > 2000 && ride < 2500 )
+{
+   return 'I will gladly take your thirty bucks.'
+}
+else if  (ride > 2500)
+{
+   return 'No can do.';
+}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if (city ==='NYC')
+  return   "Ok, sounds good."
+else return "No go."
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipe){
+  
+  if (tipe = 'if the tip is generous:') {
+    return 'Thank you so much.';
+  } else if (tipe == 'if the tip is not as generous:'){
+    return 'Thank you.';
+  } else {return 'Bye.'}
+
   // Write your code here!
 }
+
